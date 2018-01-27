a47b5a0cfda5824fb3903432cd2c987bfa0a8734
leakmemory <vinokurovdmitryy@yandex.ru>
2018-01-28 00:47:07 +0300

    Добавил абстрактный класс сущности

---
+++ BlackWhiteCubes/Entity.cpp
@@ -0,0 +1,17 @@
+﻿#include "Entity.h"
+
+
+
+Entity::Entity() : _life(true), _position(620, 100), _speedX(0), _speedY(0)
+{
+}
+
+void Entity::SetLife(bool life)
+{
+	_life = life;
+}
+
+
+Entity::~Entity()
+{
+}
---
+++ BlackWhiteCubes/Entity.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+
+class Entity
+{
+public:
+	Entity();
+	void SetLife(bool life); // задание жизни
+	virtual void Update(std::vector<sf::String>& map, const float& dt) = 0; // обновление состояния
+	virtual ~Entity();
+protected:
+	virtual void CheckCollision(std::vector<sf::String>& map, char orientation) = 0; // проверка на столкновения
+	virtual void MoveX(const float& dt) = 0; // перемещение по X
+	virtual void MoveY(const float& dt) = 0; // перемещение по Y
+protected:
+	sf::Vector2f _position; // положение на карте
+	bool _life; // жив (виден) персонаж или нет
+	float _speedX, _speedY; // скорость перемещения по X и Y
+};
+
