5ec610cb0bb5a7b8c527d9d9b05b6d9929b3ce04
leakmemory <vinokurovdmitryy@yandex.ru>
2018-01-28 00:52:24 +0300

    Добавил класс белого квадрата

---
+++ BlackWhiteCubes/WhiteCube.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Cube.h"
+
+class WhiteCube : public Cube
+{
+public:
+	WhiteCube();
+	virtual void Update(std::vector<sf::String>& map, const float& dt) override; // обновление состояния квадрата
+	~WhiteCube();
+private:
+	virtual void MoveX(const float& dt) override; // перемещение квадрата по X
+	virtual void MoveY(const float& dt) override; // перемещение квадрата по Y
+};
+
