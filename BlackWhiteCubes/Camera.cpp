a4510acc9e34744863e756bbeb00166c11fbcffd
leakmemory <vinokurovdmitryy@yandex.ru>
2018-01-29 02:28:52 +0300

    Добавил класс камеры

---
+++ BlackWhiteCubes/Camera.cpp
@@ -0,0 +1,31 @@
+﻿#include "Camera.h"
+
+
+
+Camera::Camera(const int& width, const int& height) : _view(sf::FloatRect(0, 0, width, height)), _width(width), _height(height)
+{
+
+}
+
+void Camera::Update(WhiteCube& whiteCube, Map& map)
+{
+	_position = whiteCube.GetPosition(); // позиция камеры равна позиции игрока
+
+	// при подходе к границам карты, не даем камере за них зайти
+	if (whiteCube.GetPosition().x < _width / 2) _position.x = _width / 2; // левый край
+	else if (whiteCube.GetPosition().x > map.GetWidth() - _width / 2) _position.x = map.GetWidth() - _width / 2; // правый край
+	if (whiteCube.GetPosition().y < _height / 2) _position.y = _height / 2; // верхний край
+	else if (whiteCube.GetPosition().y > map.GetHeight() - _height / 2) _position.y = map.GetHeight() - _height / 2; // нижний край
+
+	_view.setCenter(_position); // задание центра камеры
+}
+
+sf::View & Camera::GetView()
+{
+	return _view;
+}
+
+
+Camera::~Camera()
+{
+}
---
+++ BlackWhiteCubes/Camera.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include "WhiteCube.h"
+#include"Map.h"
+
+class Camera
+{
+public:
+	Camera(const int& width, const int& height); // в конструкторе задается ширина и высота камеры
+	void Update(WhiteCube& whiteCube, Map& map); // обновление позиции камеры
+	sf::View& GetView(); // возврат камеры
+	~Camera();
+private:
+	sf::View _view; // сама камера
+	sf::Vector2f _position; // позиция камеры
+	int _width, _height; // ширина и высота камеры
+};
+
