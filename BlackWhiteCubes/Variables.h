5ab5cde10408d2a81bb8ea9285b815f6445e55d1
leakmemory <vinokurovdmitryy@yandex.ru>
2018-01-29 02:31:19 +0300

    Добавил/изменил константы

--- BlackWhiteCubes/Variables.h
+++ BlackWhiteCubes/Variables.h
@@ -2,6 +2,10 @@
 const int
 CUBE_SIZE = 50, // размер квадрата (персонаж)
 MIDDLE_TILE_SIZE = 32; // размер тайла
+SPEED_X = 200, // скорость игрока по X
+SPEED_Y = -300, // скорость игрока по Y (при прыжке)
+WINDOW_WIDTH = 600, // ширина экрана
+WINDOW_HEIGHT = 640; // высота экрана
 
 const float
-ACCELERATION = 100; // ускорение
+ACCELERATION = 800; // ускорение
