17944fa477bddf54e8a049eec5340cf0721871f2
leakmemory <vinokurovdmitryy@yandex.ru>
2018-01-29 02:36:15 +0300

    Подправил небольшую ошибку

--- BlackWhiteCubes/Variables.h
+++ BlackWhiteCubes/Variables.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 const int
 CUBE_SIZE = 50, // размер квадрата (персонаж)
-MIDDLE_TILE_SIZE = 32; // размер тайла
+MIDDLE_TILE_SIZE = 32, // размер тайла
 SPEED_X = 200, // скорость игрока по X
 SPEED_Y = -300, // скорость игрока по Y (при прыжке)
 WINDOW_WIDTH = 600, // ширина экрана
